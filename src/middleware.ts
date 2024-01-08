11ff6dae52791983cae97cab47b3a980b03b2734
Amit Agarwal <amit.agarwal@ciphercru.com>
2024-01-08 18:31:10 +0530

    Ignored route

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ export default authMiddleware({
     "/api/webhook(.*)",
     "api/uploadthing",
   ],
-  ignoredRoutes: ["/api/webhooks", "api/uploadthing"],
+  ignoredRoutes: ["/api/webhook(.*)", "api/uploadthing"],
 });
 
 export const config = {
