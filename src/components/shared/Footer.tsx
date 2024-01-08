92045bb739fbf1413b33e8c3892211c5e545f3c8
Amit Agarwal <amit.agarwal@ciphercru.com>
2024-01-08 16:00:50 +0530

    Added simple Footer.

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <footer className="border-t">
+    <div className="flex-center flex-between wrapper flex flex-col gap-4 p-5 sm:flex-row">
+      <Link href="/">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={128}
+          height={34}
+        />
+      </Link>
+      <p>2023 Gather Me. All Rights Reserved.</p>
+    </div>
+  </footer>
+);
 
 export default Footer;
