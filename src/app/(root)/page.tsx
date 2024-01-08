de5bd1c0afb2ab61e417550da024ed37b9feef02
Amit Agarwal <amit.agarwal@ciphercru.com>
2024-01-08 13:11:23 +0530

    Added assets.

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Gather Me</h1>
+      <Button variant="destructive"> Sample Button </Button>
+    </main>
+  );
+}
