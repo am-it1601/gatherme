9aafa219f8eafda50a799c0ddce9ee81565eae94
Amit Agarwal <amit.agarwal@ciphercru.com>
2024-01-08 16:18:08 +0530

    Hero Section Added.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,43 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl">Gather Me</h1>
-    </div>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Host, Connect, Celebrate : Your Events, Our Platform!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book, host or join from 3168+ events happening in your locality.
+              Meet, greet and Grow your network with GatherMe !
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events"> Explore Now.</Link>
+            </Button>
+          </div>
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero_image"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section>
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">
+          Trusted by <br /> Thousand of Events.
+        </h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search Category Filter{" "}
+        </div>
+      </section>
+    </>
   );
 }
